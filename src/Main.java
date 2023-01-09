91a0cd075d0de37c934e89c7ccf388bd1ff97e3f
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-09 17:57:05 +0300

    задача2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         for(int i=0; i<11; i++)
         System.out.println(i);
+        System.out.println("ЗАДАЧА2--------------------------------");
+        for (int a=10;a>0;a--){
+            System.out.println(a);
+        }
     }
 }
