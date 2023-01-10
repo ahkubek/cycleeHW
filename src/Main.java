d4478dc101d10184fb591c98a208725989eda549
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-10 15:28:45 +0300

    задача5

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=0; i<11; i++)
-        System.out.println(i);
+        for (int i = 0; i < 11; i++)
+            System.out.println(i);
         System.out.println("ЗАДАЧА2--------------------------------");
-        for (int a=10;a>0;a--){
+        for (int a = 10; a > 0; a--) {
             System.out.println(a);
 
         }
         System.out.println("ЗАДАЧА3-------------------------------- ");
-        for (int b = 0; b<17; b=b+2){
+        for (int b = 0; b < 17; b = b + 2) {
             System.out.println(b);
         }
         System.out.println("ЗАДАЧА4----------------------------------");
-        for(int i=10; i>-11;i-- ){
+        for (int i = 10; i > -11; i--) {
             System.out.println(i);
+        }
+       System.out.println("ЗАДАЧА5----------------------------------");
+        for (int i = 1904 ; i < 2096; i = i +4) {
+            System.out.println(i+"год является високосным");
         }
 
     }
